6a8efe66b13a723796f6eae8e4876f4c66269b20
zxkuba <jakub.krzeszowiak@gmail.com>
2020-01-14 22:50:44 +0100

    zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
@@ -15,5 +16,19 @@ public class TestingMain {
         }else{
             System.out.println("Error");
         }
+
+        Calculator calculator = new Calculator();
+
+        int test1 = calculator.add(24, 64);
+        int test2 = calculator.subtract(64, 24);
+
+         int result1 = calculator.add(24, 64);
+         int result2 = calculator.subtract(64, 24);
+
+       if(test1 == result1 && test2 == result2){
+           System.out.println("alleluja i do przodu");
+       }else {
+           System.out.println("Error");
+       }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.user;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        int c;
+        c = a+b;
+        return c;
+    }
+
+    public int subtract(int a, int b){
+        int c;
+        c= a-b;
+        return c;
+    }
+}
