7189562ec88b1edfdc3a50a39e37c25591fb9f04
zxkuba <jakub.krzeszowiak@gmail.com>
2020-01-14 20:16:16 +0100

    First Commit

---
+++ src/main/java/com/kodilla/KodilaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodilaCourseApplication.class, args);
+    }
+
+}
