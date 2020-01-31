e656e8e168dbb839b6ef619abe1da01e5d7cb03b
zxkuba <jakub.krzeszowiak@gmail.com>
2020-01-31 19:39:57 +0100

    porawka

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,9 +3,10 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) {
-        if (b == 0)
+        if (b == 0) {
             throw new ArithmeticException("Nie dzielimy przez 0");
-        else
+        } else {
             return a / b;
+        }
     }
 }
